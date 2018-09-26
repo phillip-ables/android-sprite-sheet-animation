5cbb708523a48e116271e1c6553bd5c38275488e
lip ables <phillipables1@gmail.com>
2018-09-26 06:49:22 -0400

    GameView extends SurfaceView and implements runnnable class with run method implemented and super context passed

--- app/src/main/java/com/example/phill/sprite_sheets/MainActivity.java
+++ app/src/main/java/com/example/phill/sprite_sheets/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.phill.sprite_sheets;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,9 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         gameView = new GameView();  // passed the this context argument
-        setContentView(R.layout.activity_main);  // passed the gameView 
+        setContentView(R.layout.activity_main);  // passed the gameView
     }
 
-    private class GameView {
+    private class GameView extends SurfaceView implements Runnable{
+        public GameView(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void run() {
+
+        }
     }
 }
