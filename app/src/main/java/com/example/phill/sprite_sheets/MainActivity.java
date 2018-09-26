d7f9f700d6a55ab97689440ef6995b560c9b697e
lip ables <phillipables1@gmail.com>
2018-09-26 07:10:47 -0400

    reference variables type Thread, SurfaceHolder, Canvas, Paint, bitmap, some boolean and floats to keep up with variables, and 2 longs to calculate the fps

--- app/src/main/java/com/example/phill/sprite_sheets/MainActivity.java
+++ app/src/main/java/com/example/phill/sprite_sheets/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.phill.sprite_sheets;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 
@@ -16,13 +20,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        gameView = new GameView();  // passed the this context argument
-        setContentView(R.layout.activity_main);  // passed the gameView
+        gameView = new GameView(this);
+        setContentView(gameView);  // passed the R.layout.activity_main
     }
 
     private class GameView extends SurfaceView implements Runnable{
+        Thread gameThread = null;  // what is a type Thread
+
+        //We will need this when we use paint and canvas in thread
+        SurfaceHolder ourHolder;
+
+        volatile boolean playing;  // what is volatile
+        Canvas canvas;
+        Paint paint;
+        long fps;  // game frame rate
+        private long timeThisFrame;  // helps calculate the fps
+
+        //Bitmaps
+        Bitmap bitmapWorm;
+        Bitmap bitmapTurtle;
+        Bitmap bitmapBackground;
+        Bitmap bitmapstraw;
+
+        boolean isMoving = false;
+        float walkSpeedPerSecond = 250;
+
+        float wormXPosition = 10;
+        float turtleXPosition;  // will be greater then canvas length
+        float backgroundXPos = 0;
+
+        //call new special constructor method runs
         public GameView(Context context) {
-            super(context);
+            super(context);  // asks suface view to set up our object
         }
 
         @Override
