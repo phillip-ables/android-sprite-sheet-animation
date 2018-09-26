7e1c1a5404d91f151552699c29c4bd7bd0d2f503
lip ables <phillipables1@gmail.com>
2018-09-26 06:47:32 -0400

    on create make a instantiate our gameView as an inclass class

--- app/src/main/java/com/example/phill/sprite_sheets/MainActivity.java
+++ app/src/main/java/com/example/phill/sprite_sheets/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.phill.sprite_sheets;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    //the project that will hold the view
+    //and the sprite sheet animation
+    GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        gameView = new GameView();  // passed the this context argument
+        setContentView(R.layout.activity_main);  // passed the gameView 
+    }
+
+    private class GameView {
     }
 }
